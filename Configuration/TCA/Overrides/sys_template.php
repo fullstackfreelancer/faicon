0b27ed8e62a8e03506bcb3c33b3d486e6fc7f7e4
simonkohler <simonkohler@192.168.0.2>
2021-02-10 05:25:09 -0500

    Include optional TypoScript for Bootstrap Package via Overrides

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -3,17 +3,11 @@ defined('TYPO3_MODE') || die();
 
 call_user_func(function()
 {
-   /**
-    * Extension key
-    */
-   $extensionKey = 'faicon';
 
-   /**
-    * Default TypoScript
-    */
-   \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-      $extensionKey,
-      'Configuration/TypoScript/BootstrapPackage/',
-      'FAICON - Bootstrap Package Main Navigation with Icon'
-   );
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+        'faicon',
+        'Configuration/TypoScript/BootstrapPackage',
+        'Faicon: Bootstrap Package'
+    );
+
 });
